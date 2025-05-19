1968c9378755cb0f6c37e5061019e2d5573edde0
charhateom <omcharhate45@gmail.com>
2025-05-19 23:05:23 +0530

    changes

--- QRakhsa/vite.config.ts
+++ QRakhsa/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['qrakhsa-1.onrender.com']
+    
+      allowedHosts: ['qrakhsa-2.onrender.com'],
   }
 });
