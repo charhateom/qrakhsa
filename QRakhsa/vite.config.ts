75dc8dc234bddcc283afa42a00589cb2d07bdc88
charhateom <omcharhate45@gmail.com>
2025-05-19 22:56:12 +0530

    commit

--- QRakhsa/vite.config.ts
+++ QRakhsa/vite.config.ts
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-// });
-
-
-// export default defineConfig({
-//   server: {
-//     host: '0.0.0.0',
-//     port: 5173 // optional, but helps Render detect the port
-//   }
-// });
-// import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
