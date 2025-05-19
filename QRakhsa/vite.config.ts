49492f987e0e5a07408ef1b0bfa9b6ca3fd1d892
charhateom <omcharhate45@gmail.com>
2025-05-19 22:31:01 +0530

    finally

--- QRakhsa/vite.config.ts
+++ QRakhsa/vite.config.ts
@@ -10,9 +10,18 @@ import react from '@vitejs/plugin-react';
 // });
 
 
+// export default defineConfig({
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173 // optional, but helps Render detect the port
+//   }
+// });
+// import { defineConfig } from 'vite';
+
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5173 // optional, but helps Render detect the port
+    port: 5173,
+    allowedHosts: ['qrakhsa-1.onrender.com']
   }
 });
